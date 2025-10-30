fdb96cd6a740da6a543999afa6131852885ed7cc
ricotandrio <ricotandrio@gmail.com>
2025-10-30 14:34:44 +0700

    Initial Commit

---
+++ RiSTOCK-Client/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RiSTOCK-Client
+//
+//  Created by Rico Tandrio on 30/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RiSTOCK-Client/RiSTOCK_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiSTOCK_ClientApp.swift
+//  RiSTOCK-Client
+//
+//  Created by Rico Tandrio on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RiSTOCK_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
