e2e284402d11bf0e571d7d8f51f1f54811595493
ricotandrio <ricotandrio@gmail.com>
2025-11-20 14:35:23 +0700

    feat(inter): introduce inter font family:

--- RiSTOCK-Client/Common/Fonts/UIFont+ext.swift
+++ RiSTOCK-Client/Common/Fonts/UIFont+ext.swift
@@ -14,14 +14,23 @@ extension UIFont {
         case primary = "Inter"
     }
     
-    static func resolveFontName(for family: FontFamily, weight: UIFont.Weight) -> String {
+    static func resolveFontName(
+        for family: FontFamily,
+        weight: UIFont.Weight
+    ) -> String {
         switch (family, weight) {
         case (.primary, .regular): 
-            return "Inter-Regular"
+            return "Inter-Black"
         case (.primary, .semibold):
             return "Inter-SemiBold"
         case (.primary, .bold):
             return "Inter-Bold"
+        case (.primary, .medium):
+            return "Inter-Medium"
+        case (.primary, .thin):
+            return "Inter-Thin"
+        case (.primary, .light):
+            return "Inter-Light"
         default:
             return "\(family.rawValue)-Regular"
         }
