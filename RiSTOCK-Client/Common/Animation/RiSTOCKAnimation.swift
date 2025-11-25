329653c3b4ff2bb2e7a955739894efb6591ae1e9
Andrea Octaviani <andrea.octaviani12@gmail.com>
2025-11-25 17:20:55 +0700

    feat(row): update row component and add lottie animation support [DEV-174]
    Integrated Lottie by adding the lottie-ios Swift package and updating project configuration. Added Fire.json animation asset and RiSTOCKAnimation.swift for handling animations. Updated ProductRow and ProductStockView to support new animation features.

---
+++ RiSTOCK-Client/Common/Animation/RiSTOCKAnimation.swift
@@ -0,0 +1,12 @@
+//
+//  RiSTOCKAnimation.swift
+//  RiSTOCK-Client
+//
+//  Created by Andrea Octaviani on 24/11/25.
+//
+
+import Lottie
+
+enum RiSTOCKAnimation {
+    static let fire: LottieAnimation? = .named("Fire.json")
+}
